9a718945922b948f8fba97041993e3d0d084d8f7
Jaymee Liu <{jaymeeliu@gmail.com}>
2020-04-19 19:50:25 -0700

    deploy adjustments

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,7 @@ import { WebSocketLink } from "apollo-link-ws";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:4000/graphql',
+   uri: process.env.NODE_ENV === 'production' ? 'wss://geolocate20.herokuapp.com/graphql' : 'ws://localhost:4000/graphql',
   options: {
     reconnect: true
   }
